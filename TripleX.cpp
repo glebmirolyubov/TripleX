1b6ec2f45f40406b62c4d970f429579b3eb2f6ba
Gleb Mirolyubov <gleb.mirolyubov@gmail.com>
2021-03-10 21:21:36 +0100

    Further additions to the game

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,6 +2,7 @@
 
 class GameText 
 {
+    // Prints messages at the start of the game to the console
     public: static void printStartMessages()
     {    
         std::cout << std::endl;
@@ -11,16 +12,33 @@ class GameText
         std::cout << std::endl << std::endl;
     }
 
+    // Prints the product of three variables to the console
     public: static void printVariablesProduct()
     {
-        int a = 4;
-        int b = 5;
-        int c = 6;
+        const int CodeA = 4;
+        const int CodeB = 5;
+        const int CodeC = 6;
 
-        int product = a * b * c;
+        const int CodeSum = CodeA + CodeB + CodeC;
+        const int CodeProduct = CodeA * CodeB * CodeC;
 
-        std::cout << product;
-        std::cout << std::endl;
+        std::cout << "There are 3 numbers in the code" << std::endl;
+        std::cout << "The codes add-up to: " << CodeSum << std::endl;
+        std::cout << "The codes multiply to give: " << CodeProduct << std::endl << std::endl;
+    }
+
+    public: static void getPlayerGuessCode()
+    {
+        int GuessA, GuessB, GuessC;
+
+        std::cout << "Enter your guess: ";
+        std::cin >> GuessA;
+        std::cin >> GuessB;
+        std::cin >> GuessC;
+        std::cout << std::endl << "You entered: " << GuessA << GuessB << GuessC << std::endl << std::endl;
+
+        int GuessSum = GuessA + GuessB + GuessC;
+        int GuessProduct = GuessA * GuessB * GuessC;
     }
 };
 
@@ -30,6 +48,7 @@ int main ()
 
     g.printStartMessages();
     g.printVariablesProduct();
+    g.getPlayerGuessCode();
 
     return 0;
 }
