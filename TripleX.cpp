bae94e066b4490158f190afa1305c89ead4956fc
Gleb Mirolyubov <gleb.mirolyubov@gmail.com>
2021-03-10 17:55:19 +0100

    Written hello world and created basic variables

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,35 @@
+#include <iostream>
+
+class GameText 
+{
+    public: static void printStartMessages()
+    {    
+        std::cout << std::endl;
+        std::cout << "You are a secret agent breaking into a secure server room";
+        std::cout << std::endl;
+        std::cout << "You need to enter the correct codes to continue...";
+        std::cout << std::endl << std::endl;
+    }
+
+    public: static void printVariablesProduct()
+    {
+        int a = 4;
+        int b = 5;
+        int c = 6;
+
+        int product = a * b * c;
+
+        std::cout << product;
+        std::cout << std::endl;
+    }
+};
+
 int main ()
 {
+    GameText g;
+
+    g.printStartMessages();
+    g.printVariablesProduct();
+
     return 0;
 }
